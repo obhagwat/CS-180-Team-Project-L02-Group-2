e33eb8430b7a293420260e28fb13f8146043c568
Ana Farmus <anafar@pal-nat186-130-58.itap.purdue.edu>
2025-04-02 15:07:14 -0400

    added database class

---
+++ src/src/Database.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Database {
+    private ArrayList<User> users;
+    private ArrayList<Chat> chats;
+    private static final Object GATEKEEPER = new Object();
+}
