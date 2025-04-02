06894e3792b90346665c91ca6c6748d1be859315
Ana Farmus <anafar@pal-nat186-130-58.itap.purdue.edu>
2025-04-02 15:48:11 -0400

    more doc

--- src/src/InvalidBidException.java
+++ src/src/InvalidBidException.java
@@ -1,3 +1,9 @@
+/**
+ * InvalidBid Exception: Contains invalid Bid objects
+ *  @author Sarah Stone, Lab sec 02
+ *
+ *  @version Apr --, 2025
+ */
 public class InvalidBidException extends Exception {
     public InvalidBidException(String message) {
         super(message);
