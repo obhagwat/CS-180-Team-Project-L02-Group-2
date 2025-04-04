f6db8a9f60d6a1b9536b9ee613d542acc8682a80
Ana Farmus <anafar@pal-nat186-137-216.itap.purdue.edu>
2025-04-04 15:54:02 -0400

    added db constructor

--- src/src/Database/Database.java
+++ src/src/Database/Database.java
@@ -15,10 +15,27 @@ public class Database implements DatabaseInterface {
     private ArrayList<Chat> chats;
     private static final Object GATEKEEPER = new Object();
 
+/**
+ * Constructs a Database object initialized with a given list of users.
+ * This constructor is used internally for singleton instantiation.
+ *
+ * @param users the list of users to initialize the database with
+ * @param chats the list of chats to initialize the database with
+ */
     public Database(ArrayList<User> users, ArrayList<Chat> chats) {
         synchronized (GATEKEEPER) {
             this.users = users;
             this.chats = chats;
         }
+    }
+
+    /**
+     * Constructs an empty Database and loads data from the specified file
+     * into the users list.
+     */
+    public Database() {
+        this.users = new ArrayList<>();
+        this.chats = new ArrayList<>();
+//        loadDatabase();
     }
 }
