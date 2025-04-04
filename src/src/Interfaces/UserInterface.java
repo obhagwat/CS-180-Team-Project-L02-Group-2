44fa871e6f2ff4884185da362ba1bfb2f43da44f
Sarah Stone <sarahstone@Sarahs-MacBook-Air-8.local>
2025-04-03 22:01:14 -0400

    user interface

--- src/src/Interfaces/UserInterface.java
+++ src/src/Interfaces/UserInterface.java
@@ -1,5 +1,8 @@
 package Interfaces;
 
+import Objects.Chat;
+import Objects.User;
+
 /**
  * User Interface: Interface for User class
  *  @author Sarah Stone, Lab sec 02
@@ -21,4 +24,18 @@ public interface UserInterface {
     String getEmail();
     String getPhoneNumber();
     double getBalance();
+    void setUsername(String username);
+    void setPassword(String password);
+    void setRating(double rating);
+    void setNumOfRatings(int numOfRatings);
+    void setCountryOfOrigin(String countryOfOrigin);
+    void setAddress(String address);
+    void setEmail(String email);
+    void setPhoneNumber(String phoneNumber);
+    void setBalance(double balance);
+    void deleteAccount();
+    void sendMessage(Chat chat, String message);
+    void postReview(User user, int rating);
+    void calcRating(int newRating);
+    void calcIntRating();
 }
