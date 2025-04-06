160dd426a928755ca3c590c59cc4ae061c39c820
anafarmus1 <afarmus@purdue.edu>
2025-04-06 15:19:33 -0400

    Update DatabaseInterface.java

--- src/src/Interfaces/DatabaseInterface.java
+++ src/src/Interfaces/DatabaseInterface.java
@@ -1,10 +1,1 @@
-package Interfaces;
 
-/**
- *  Database Interface: Interface for Database class
- *  @author Sarah Stone, Lab sec 02
- *  @version Apr 6, 2025
- */
-
-public interface DatabaseInterface {
-}
