91de55ab34f60aca9b5e28adc3593767a4915d29
Saahil Kajarekar <saahilkajarekar@Dont-bother-me-please.local>
2025-04-17 00:31:06 -0400

    ServerInterface

--- src/src/NetworkIO/Server.java
+++ src/src/NetworkIO/Server.java
@@ -1,9 +1,38 @@
 package NetworkIO;
 
+import Objects.*;
+import Database.*;
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * Server Class: Contains all methods and fields pertaining to server objects
- *  @author Ana Farmus, Lab sec 02
+ *  @author Ana Farmus, Saahil Kajarekar, Lab sec 02
  *  @version Apr 20, 2025
  */
-public class Server {
+abstract class Server implements ServerInterface,Runnable {
+    private final int PORT = 888;
+    private ServerSocket serverSocket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private Database DB = Database.getInstance();
+
+    /**
+     * Creates the server and waits for the client to be ran
+     */
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + PORT);
+        }
+    }
+
+    public void run(){
+        Server server = this;
+        //server.acceptConnection();
+    }
+
+
 }
--- src/src/NetworkIO/ServerInterface.java
+++ src/src/NetworkIO/ServerInterface.java
@@ -1,9 +1,72 @@
 package NetworkIO;
 
+import Objects.*;
+import Exceptions.*;
+import java.util.*;
 /**
  * Server Interface Class: Contains all methods and fields pertaining to server objects
- *  @author Ana Farmus, Lab sec 02
- *  @version Apr 6, 2025
+ * Note: Some of these will be one line because they are handled by the database
+ * and we just need to propogate it up
+ *
+ * @author Saahil Kajarekar, Lab sec 02
+ * @version Apr 16, 2025
  */
-public interface ServerInterface {
+public interface ServerInterface{
+
+    /**
+     * accepts a message from the Client
+     * @return the message from the client
+     */
+    String readFromClient();
+
+    /**
+     * sends a string to the Client
+     * @param message the message to be sent
+     */
+    void sendToClient(String message);
+
+    /**
+     * authenticates a user through their username and password
+     * @throws InvalidUserException if the user is not authenticated
+     */
+    boolean authenticateUser();
+
+    /**
+     *Sends a message from one client to another
+     * @param message the message to be sent
+     */
+    void delieverToClient(String message);
+    //Database based
+    /**
+     * creates a user that will be stored in the database
+     * @return true if the user was sucessfully created else false
+     * @throws InvalidUserException  if the use object is invalid
+     */
+    boolean createUser();
+    /**
+     * deletes a user from the database
+     * @return true if the user was deleted sucessfully else false
+     * @throws InvalidUserException if the user object is invalid
+     * @throws InvalidChatException if the chat involves a deleted user
+     */
+    boolean deleteUser();
+
+    //GUI based
+    /**
+     * displays all contracts for the industry of the contractor
+     */
+    void displayPossibleContracts();
+    /**
+     * display contractors on the GUI of our choosing
+     */
+    void displayContractors();
+    /**
+     * Allows the client to modify their profile including username and password
+     */
+    void editProfile();
+    /**
+     *
+     */
+
+
 }
