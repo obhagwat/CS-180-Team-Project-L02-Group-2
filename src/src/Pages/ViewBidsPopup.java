66736ce960a27a75cf4bc4d227c694daee7dd8a4
Ovi Bhagwat <ovib@outlook.com>
2025-05-03 16:09:55 -0400

    modded homepages and added helper popups and pages

--- src/src/Pages/ViewBidsPopup.java
+++ src/src/Pages/ViewBidsPopup.java
@@ -10,11 +10,11 @@ import java.util.List;
 
 public class ViewBidsPopup {
     public ViewBidsPopup(Client client, Contract contract) {
-//        List<Bid> bids = client.getSolicitor().getContractsSolicited();
-//        StringBuilder sb = new StringBuilder();
-//        for (Bid bid : bids) {
-//            sb.append(bid.getContractor().getContractorName()).append(" - $").append(bid.getRequestedPay()).append("\n");
-//        }
-//        JOptionPane.showMessageDialog(null, sb.toString(), "Submitted Bids", JOptionPane.INFORMATION_MESSAGE);
+        List<Bid> bids = client.getSolicitor().getContractsSolicited();
+        StringBuilder sb = new StringBuilder();
+        for (Bid bid : bids) {
+            sb.append(bid.getContractor().getContractorName()).append(" - $").append(bid.getRequestedPay()).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, sb.toString(), "Submitted Bids", JOptionPane.INFORMATION_MESSAGE);
     }
 }
