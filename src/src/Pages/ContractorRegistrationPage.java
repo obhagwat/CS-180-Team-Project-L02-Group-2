bd7be9ad13716798a123b679af07f06e94bddef4
Ana Farmus <anafar@Anas-MacBook-Pro-2.local>
2025-05-04 15:37:23 -0400

    registration fix

--- src/src/Pages/ContractorRegistrationPage.java
+++ src/src/Pages/ContractorRegistrationPage.java
@@ -68,7 +68,7 @@ public class ContractorRegistrationPage extends Page implements PageInterface {
 
         //continueButton = new Button("Continue",
         //        e -> window.switchPage(new ContractorPaymentInfoPage(client)), Constants.SIZE_500_45);
-        continueButton = new Button("Continue", e -> register(), Constants.SIZE_500_45);
+        continueButton = new Button("Continue", e -> window.switchPage(new ContractorPaymentInfoPage(client)), Constants.SIZE_500_45);
         goBackButton = new TransparentButton("Go Back",
                 e -> window.switchPage(new RegisterPage(client)), Constants.SIZE_500_45);
 
